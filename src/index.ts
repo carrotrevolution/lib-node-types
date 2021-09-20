22882b6b86f14e610bc92c44110a3a089f3d1624
Tero Saarikivi <tero.saarikivi@slush.org>
2021-09-20 16:29:19 +0300

    chore: sendgrid

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './client-platform';
 export * from './language';
 export * from './revenue-cat';
+export * from './sendgrid';
---
+++ src/sendgrid.ts
@@ -0,0 +1,16 @@
+import {SupportedLanguage} from './language';
+
+type SendgridListItem = {[lang in SupportedLanguage]: string};
+
+export const SendgridList: {
+  [id: string]: SendgridListItem;
+} = {
+  MARKETING: {
+    [SupportedLanguage.FI]: 'b3c1d82f-e229-4fc7-af30-8fe1c62d02c5',
+    [SupportedLanguage.EN]: 'b3c1d82f-e229-4fc7-af30-8fe1c62d02c5',
+  },
+  NEW_SUBSCRIBERS: {
+    [SupportedLanguage.FI]: '0b7ae9bf-8b30-4187-9f92-d154354a0297',
+    [SupportedLanguage.EN]: '170ed6f5-b886-4bf6-bab2-79c343bae8dd',
+  },
+};
