9c7e4e26742998a512c8220eba09c0e4a12e733a
Tero Saarikivi <tero.saarikivi@slush.org>
2021-10-21 15:47:19 +0300

    chore: gcode metadata

--- src/gift-code.ts
+++ src/gift-code.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import {SupportedLanguage} from './locale';
 import {RevenueCatDuration} from './revenue-cat';
 
@@ -41,6 +43,7 @@ export interface GiftCode {
   sendTo?: GiftCodeSendTo;
   downloadURL?: string;
   message?: string;
+  metadata?: any;
 }
 
 export interface GiftCodeSendTo {
