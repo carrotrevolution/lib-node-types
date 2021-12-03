562d914af81ceecbc2a722ba32d65c4782147cd8
Tero Saarikivi <saarikivit@gmail.com>
2021-12-03 17:10:13 +0200

    chore: search result

--- src/search.ts
+++ src/search.ts
@@ -4,3 +4,12 @@ export interface SearchInput {
   page: number;
   hitsPerPage: number;
 }
+
+export interface SearchResult<T> {
+  results?: T;
+  page: number;
+  hitsPerPage: number;
+  nbHits: number;
+  nbPages: number;
+  isLastPage: boolean;
+}
