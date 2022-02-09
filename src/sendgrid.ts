0916bb6bb55251595dd714e17d119f422dfffa3b
Tero Saarikivi <saarikivit@gmail.com>
2022-02-09 13:46:15 +0200

    test: fix

--- src/sendgrid.ts
+++ src/sendgrid.ts
@@ -8,9 +8,11 @@ export const SendgridList: {
   MARKETING: {
     [SupportedLanguage.FI]: 'b3c1d82f-e229-4fc7-af30-8fe1c62d02c5',
     [SupportedLanguage.EN]: 'b3c1d82f-e229-4fc7-af30-8fe1c62d02c5',
+    [SupportedLanguage.EN_US]: 'b3c1d82f-e229-4fc7-af30-8fe1c62d02c5',
   },
   NEW_SUBSCRIBERS: {
     [SupportedLanguage.FI]: '0b7ae9bf-8b30-4187-9f92-d154354a0297',
     [SupportedLanguage.EN]: '170ed6f5-b886-4bf6-bab2-79c343bae8dd',
+    [SupportedLanguage.EN_US]: '170ed6f5-b886-4bf6-bab2-79c343bae8dd',
   },
 };
